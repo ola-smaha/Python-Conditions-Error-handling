c7a4d08d69bccb89c11872648535d21e931ccfa6
ola-smaha <olasmaha@gmail.com>
2023-09-07 21:23:01 +0300

    handled errors in pandas_handler

--- Ola_ETL_Proj/lookups.py
+++ Ola_ETL_Proj/lookups.py
@@ -1,14 +1,19 @@
 from enum import Enum
 
 class Errors(Enum):
-    CONNECTION_ERROR = "Error connecting to the database. "
-    RETURN_QUERY_ERROR = "Error: Cannot return query. "
-    CSV_RETURN_ERROR = "Error: can't return csv file. "
-    SQL_RETURN_ERROR = "Error: can't return sql query. "
-    EXCEL_RETURN_ERROR = "Error: can't return excel file. "
-    UNDEFINED_DATA_ERROR = "Error: data type is undefined."
-    NO_ERROR = "Executed Successfully."
-    EXECUTION_ERROR = "Error executing query. "
+    CONNECTION_ERROR = "Error connecting to the database, "
+    RETURN_QUERY_ERROR = "Error: Cannot return query, "
+    CSV_RETURN_ERROR = "Error: can't return csv file, "
+    SQL_RETURN_ERROR = "Error: can't return sql query, "
+    EXCEL_RETURN_ERROR = "Error: can't return excel file, "
+    UNDEFINED_DATA_ERROR = "Error: data type is undefined,"
+    NO_ERROR = "Executed Successfully,"
+    EXECUTION_ERROR = "Error executing query, "
+    REMOVE_DUPLICATES_ERROR = "Error removing duplicates, "
+    REMOVE_NULLS_ERROR = "Error removing null values, "
+    GET_BLANKS_ERROR = "Error getting nan values, "
+    GET_SHAPE_ERROR = "Error getting dataframe shape, "
+    GET_LENGTH_ERROR = "Error getting counts, "
 
 
 class Types(Enum):
--- Ola_ETL_Proj/pandas_handler.py
+++ Ola_ETL_Proj/pandas_handler.py
@@ -1,15 +1,33 @@
 import pandas as pd
-from lookups import PandasFunctions
+from lookups import PandasFunctions, Errors
+from logging_handler import display_error
 
 def pandas_function(df,function):
-    if function == PandasFunctions.REMOVE_DUPLICATES:
-        return df.drop_duplicates()
-    elif function == PandasFunctions.REMOVE_NULLS:
-        return df.dropna()
-    elif function == PandasFunctions.GET_BLANKS:
-        return df[df.isna()]
-    elif function == PandasFunctions.GET_SHAPE:
-        return df.shape
-    elif function == PandasFunctions.GET_LENGTH:
-        return df.count()
+    try:
+        if function == PandasFunctions.REMOVE_DUPLICATES:
+            return df.drop_duplicates()
+        elif function == PandasFunctions.REMOVE_NULLS:
+            return df.dropna()
+        elif function == PandasFunctions.GET_BLANKS:
+            return df[df.isna()]
+        elif function == PandasFunctions.GET_SHAPE:
+            return df.shape
+        elif function == PandasFunctions.GET_LENGTH:
+            return df.count()
+        else:
+            raise Exception("Failed. Use pandas function manually.")
+    except Exception as e:
+        error_msg = str(e)
+        if function == PandasFunctions.REMOVE_DUPLICATES:
+            enum_error = Errors.REMOVE_DUPLICATES_ERROR.value
+        elif function == PandasFunctions.REMOVE_NULLS:
+            enum_error = Errors.REMOVE_NULLS_ERROR.value
+        elif function == PandasFunctions.GET_BLANKS:
+            enum_error = Errors.GET_BLANKS_ERROR.value
+        elif function == PandasFunctions.GET_SHAPE:
+            enum_error = Errors.GET_SHAPE_ERROR
+        elif function == PandasFunctions.GET_LENGTH:
+            enum_error = Errors.GET_LENGTH_ERROR
+        display_error(enum_error,error_msg)
+
     
